8a47e68d5c5164b4500dd2d7251c27cbe5f0332e
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-05 02:02:42 -0500

    aggregate

--- component_parameter_analysis/saved.sh
+++ component_parameter_analysis/saved.sh
@@ -1,1 +1,3 @@
 for i in mapreduce/*.txt; do ./filter.sh $i > mapreduce/filter/$(echo $i | awk -F '/' '{print $2}'); done
+
+for proj in hdfs hbase yarn mapreduce; do get_funcs=$(ls "$proj"/*txt | awk -F '_' '{print $1}' | sort -u); for func in ${get_funcs[@]}; do echo "$func"; cat "$func"*; done; done
